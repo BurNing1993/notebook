fd886916a25da10567e665209fa0377591483dda
Joey <786678257@qq.com>
2019-09-15 20:27:49 +0800

    up pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,5 +3,15 @@ module.exports = {
   description: 'Notebook',
   base: '/notebook/',
   themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Vue', link: '/vue/' },
+      { text: 'React', link: '/react/' },
+      { text: 'TypeScript', link: '/typescript/' },
+      { text: 'Tools', link: '/tools/' },
+      { text: 'Other', link: '/other/' },
+      { text: 'Github', link: 'https://github.com/BurNing1993/notebook' },
+    ]
   }
 }
